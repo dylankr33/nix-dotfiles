5358dba4d08cc21164eff5a4c2b6d7e3e08b3153
dylank <dylank@posteo.com>
2026-03-18 18:20:06 -0500

    moved assets to dwalls

--- pkgs/dWalls/default.nix
+++ pkgs/dWalls/default.nix
@@ -4,7 +4,7 @@
 pkgs.stdenvNoCC.mkDerivation {
   pname = "dWalls";
   version = "1";
-  src = ../assets;
+  src = ./assets;
   buildPhase = ''
     mkdir -p $out/share
     cp -r $src $out/share/wallpapers
